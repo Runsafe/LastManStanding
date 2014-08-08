2414807f86f98fa0e74074cef22b0ce876583165
Kruithne <kruithne@gmail.com>
2014-08-08 18:34:41 +0100

    Initialized new plugin LastManStanding from template

---
+++ src/no/runsafe/lastmanstanding/LastManStanding.java
@@ -0,0 +1,26 @@
+package no.runsafe.lastmanstanding;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class LastManStanding extends RunsafePlugin
+//public class LastManStanding extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
