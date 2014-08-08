fb42cd2a97e79d3a4ab79644dac2269d98d35171
Kruithne <kruithne@gmail.com>
2014-08-08 18:37:12 +0100

    Clean up commented templating, set as configurable plug-in.

--- src/no/runsafe/lastmanstanding/LastManStanding.java
+++ src/no/runsafe/lastmanstanding/LastManStanding.java
@@ -1,11 +1,10 @@
 package no.runsafe.lastmanstanding;
 
-import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.RunsafeConfigurablePlugin;
 
-public class LastManStanding extends RunsafePlugin
-//public class LastManStanding extends RunsafeConfigurablePlugin
+public class LastManStanding extends RunsafeConfigurablePlugin
 {
-//	public static IDebug Debugger = null;
+	//	public static IDebug Debugger = null;
 
 	@Override
 	protected void pluginSetup()
@@ -13,14 +12,11 @@ public class LastManStanding extends RunsafePlugin
 //		Debugger = getComponent(IDebug.class);
 
 		// Framework features
-//		addComponent(Commands.class);
-//		addComponent(Database.class);
-//		addComponent(Events.class);
-//		addComponent(FrameworkHooks.class);
-//		addComponent(LUAScripts.class);
-//		addComponent(UniverseRegistration.class);
-
-		// Plugin components
-//		addComponent(SomeComponent.class);
+		//addComponent(Commands.class);
+		//addComponent(Database.class);
+		//addComponent(Events.class);
+		//addComponent(FrameworkHooks.class);
+		//addComponent(LUAScripts.class);
+		//addComponent(UniverseRegistration.class);
 	}
 }
